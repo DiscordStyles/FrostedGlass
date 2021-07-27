90ac05f4f4e7eea57033fa6e4e0bade4ea4e32c1
Gibbu <Gibbu_@hotmail.com>
2021-07-27 12:52:55 +1000

    Updated for new repo structure

---
+++ scripts/build.js
@@ -0,0 +1,32 @@
+// Used to build scss files to the dist folder.
+
+const sass = require('sass');
+const fs = require('fs');
+const postcss = require('postcss');
+const autoprefixer = require('autoprefixer');
+
+console.log('Building FrostedGlass.css file...');
+
+sass.render({
+	file: 'src/_base.scss',
+	outputStyle: 'expanded'
+}, (err, result) => {
+	if (err) {
+		console.error(err);
+		return false;
+	}
+
+	const newRes = Buffer.from(result.css).toString();
+
+	postcss([autoprefixer])
+		.process(newRes, {
+			from: undefined,
+			to: undefined
+		})
+		.then(postcssRes => {
+			fs.writeFile('dist/FrostedGlass.css', postcssRes.css, (err) => {
+				if (err) console.error(err);
+				else console.log(`Successfully built FrostedGlass.css file. (${(result.stats.duration/60000 * 60).toFixed(2)}s)`);
+			})
+		})
+})
---
+++ scripts/dev.js
@@ -0,0 +1,38 @@
+// Used to build scss files to the BetterDiscord themes folder.
+
+const chokidar = require('chokidar');
+const sass = require('sass');
+const fs = require('fs');
+const postcss = require('postcss');
+const autoprefixer = require('autoprefixer');
+
+const dataFolder = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Preferences' : process.env.HOME + "/.local/share");
+const themesFolder = dataFolder + '\\BetterDiscord\\themes';
+
+chokidar.watch('src', {persistent: true})
+	.on('ready', () => console.log('Watching for changes...'))
+	.on('change', () => {
+		sass.render({
+			file: 'src/FrostedGlass.theme.scss',
+			outputStyle: 'expanded'
+		}, (err, result) => {
+			if (err) {
+				console.error(err);
+				return false;
+			}
+
+			const newRes = Buffer.from(result.css).toString();
+		
+			postcss([autoprefixer])
+				.process(newRes, {
+					from: undefined,
+					to: undefined
+				})
+				.then(postcssRes => {
+					fs.writeFile(themesFolder+'\\FrostedGlass.theme.css', postcssRes.css, (err) => {
+						if (err) console.error(err);
+						else console.log(`Built css file. (${(result.stats.duration/60000 * 60).toFixed(2)}s)`);
+					})
+				})
+		})
+	})
