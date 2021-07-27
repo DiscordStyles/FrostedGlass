b9dc6d90ef3c2000d06399cc53b5c1a158b477f2
Gibbu <Gibbu_@hotmail.com>
2021-07-27 15:21:01 +1000

    Fixed dev script

--- scripts/dev.js
+++ scripts/dev.js
@@ -3,11 +3,12 @@
 const chokidar = require('chokidar');
 const sass = require('sass');
 const fs = require('fs');
+const path = require('path');
 const postcss = require('postcss');
 const autoprefixer = require('autoprefixer');
 
-const dataFolder = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Preferences' : process.env.HOME + "/.local/share");
-const themesFolder = dataFolder + '\\BetterDiscord\\themes';
+const dataFolder = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Application Support' : process.env.HOME + "/.local/share");
+const themesFolder = path.join(dataFolder, 'BetterDiscord', 'themes');
 
 chokidar.watch('src', {persistent: true})
 	.on('ready', () => console.log('Watching for changes...'))
@@ -29,7 +30,7 @@ chokidar.watch('src', {persistent: true})
 					to: undefined
 				})
 				.then(postcssRes => {
-					fs.writeFile(themesFolder+'\\FrostedGlass.theme.css', postcssRes.css, (err) => {
+					fs.writeFile(path.join(themesFolder, 'FrostedGlass.theme.css'), postcssRes.css, (err) => {
 						if (err) console.error(err);
 						else console.log(`Built css file. (${(result.stats.duration/60000 * 60).toFixed(2)}s)`);
 					})
