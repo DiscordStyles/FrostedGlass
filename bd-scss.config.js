ff9811259e384b8adb1c6b64cc36feb4de599c84
Ian <Gibbu_@hotmail.com>
2022-09-27 00:34:50 +1000

    Big updates

---
+++ bd-scss.config.js
@@ -0,0 +1,17 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+	meta: {
+		name: 'Frosted Glass',
+		author: 'Gibbu#1211',
+		version: '2.1.0',
+		description: 'Display your picture of choice with adjustable blur and brightness. Dark theme is required.',
+		invite: 'ZHthyCw',
+		authorId: '174868361040232448',
+		source: 'https://github.com/DiscordStyles/FrostedGlass',
+		website: 'https://gibbu.me/',
+	},
+	base: {
+		output: 'dist/dist'
+	},
+	baseImport: 'https://discordstyles.github.io/FrostedGlass/dist/FrostedGlass.css'
+}
