6af784a96c0ea7a9d445ee075a7953379e0bc37d
Gibbu <me@gibbu.dev>
2025-04-22 21:29:38 +1000

    Bump deps

--- bd-scss.config.js
+++ bd-scss.config.js
@@ -2,16 +2,11 @@
 export default {
 	meta: {
 		name: 'Frosted Glass',
-		author: 'Gibbu#1211',
+		author: 'Gibbu',
 		version: '2.2.0',
 		description: 'Display your picture of choice with adjustable blur and brightness. Dark theme is required.',
-		invite: 'ZHthyCw',
-		authorId: '174868361040232448',
 		source: 'https://github.com/DiscordStyles/FrostedGlass',
-		website: 'https://gibbu.me/'
+		invite: 'ZHthyCw'
 	},
-	base: {
-		output: 'dist/dist'
-	},
-	baseImport: 'https://discordstyles.github.io/FrostedGlass/dist/FrostedGlass.css'
+	github: 'DiscordStyles'
 };
